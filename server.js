b28e631f6f8e1e86d6dd7007844d2910c3790d10
Mathieu Francois <mathieu.francois@enseirb-matmeca.fr>
2024-12-04 16:31:43 +0100

    final server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');  //Pour psql
 const app = express();
-const port = 8080;  
+const port = process.env.PORT || 10000;
 
 
 
